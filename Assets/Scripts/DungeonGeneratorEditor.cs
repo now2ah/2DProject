212107fdf4469cf4873c9df92f59493f42ba9886
now2ah <hhw3287@hanmail.net>
2025-04-18 16:00:02 +0900

    추가 커밋

---
+++ Assets/Scripts/DungeonGeneratorEditor.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(DungeonGenerator))]
+public class DungeonGeneratorEditor : Editor
+{
+    DungeonGenerator dungeonGenerator;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        dungeonGenerator = (DungeonGenerator)target;
+
+        EditorGUILayout.BeginVertical();
+
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Generate Dungeon"))
+        {
+            dungeonGenerator.GenerateDungeon();
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+}
